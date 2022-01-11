c68db683f9571f6684c282fe3ba639e9199f873a
heyji-ten <59608643+heyji-ten@users.noreply.github.com>
2022-01-11 23:29:27 +0900

    portfolio page 계획

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,19 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* 포트폴리오 사이트에 들어가야할 것
+      1. 이력서 pdf 다운로드
+      2. 자기소개 (연락처 등 포함)
+      3. 기술 스택 
+        3-1. 다룰 수 있는 기술 (HTML : 중, CSS : 중, JS : 하, REACT : 하)
+      4. 교육 이력 (인터넷 강의 등)
+      5. 프로젝트
+        5-1. 깃허브 및 프리뷰 링크
+        5-2. 작업하면서 막혔던 부분, 해결한 방법 간단하게 보기좋게 입력 */}
+      <header>
+
       </header>
     </div>
   );
